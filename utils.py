5874c5852e6346c491d5d58c1f530739ad60705e
FarhatN <farhat_ink@mail.ru>
2022-06-16 18:41:38 +0500

    функция для работы с соседней клеткой

--- utils.py
+++ utils.py
@@ -8,3 +8,10 @@ def randcell(w, h):
     tw = rand(0, w)
     th = rand(0, h)
     return (th, tw)
+
+# 0 - наверх, 1 - направо, 2 - вниз, 3 - налево
+def randcell2(x, y):
+    moves = [(-1, 0) (0, 1) (1, 0) (0, -1)]
+    t = rand(0, 4)
+    dx, dy = moves[t][0], moves[t][1]
+    return (x + dx, y + dy)
