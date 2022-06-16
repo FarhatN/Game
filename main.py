423491f3ee7eccd2ff1703d76ce9d8c431a60104
FarhatN <farhat_ink@mail.ru>
2022-06-16 20:27:58 +0500

    добавлена механика для огня

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ import os
 
 TICK_SLEEP = 0.05
 TREE_UPDATE = 50
+FIRE_UPDATE = 25
 
-tmp = Map(20, 10)
-tmp.generate_forest(3, 10)
-tmp.generate_river(10)
-tmp.generate_river(10)
-tmp.add_fire()
-tmp.print_map()
+field = Map(20, 10)
+field.generate_forest(3, 10)
+field.generate_river(10)
+field.generate_river(10)
+field.print_map()
 
 tick = 1
 
 while True:
     os.system('cls')
     print('TICK', tick)
-    tmp.print_map()
+    field.print_map()
     tick += 1
     time.sleep(TICK_SLEEP)
     if (tick % TREE_UPDATE == 0):
-        tmp.generate_tree()
+        field.generate_tree()
+    if (tick % FIRE_UPDATE == 0):
+        field.update_fires()
