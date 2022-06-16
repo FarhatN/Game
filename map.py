bf360181f7047c189e01a4408d86c164748e89f9
FarhatN <farhat_ink@mail.ru>
2022-06-16 15:03:05 +0500

    добавлена функция для вывода карты

--- map.py
+++ map.py
@@ -3,9 +3,20 @@
 # 2 - река
 
 class Map:
-    def generate_rivers():
+    #def generate_rivers():
 
-    def generate_forest():
+    #def generate_forest():
+
+    def print_map():
+        for row in self.cells:
+            for cell in row:
+                if cell == 0:
+                    print('❎', end ='')
+                elif cell == 1:
+                    print('🌳', end ='')
+                elif cell == 2:
+                    print('🌊', end = '')
+            print()
 
     def __init__(self, w, h):
         self.cells = [[0 for i in range(w)] for j in range(h)]
