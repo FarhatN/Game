f268593ecd3016afb1de7dd17b43acde8962434e
FarhatN <farhat_ink@mail.ru>
2022-06-16 19:59:55 +0500

    добавлена отрисовка кадров

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 # 🌳 🌊 🚁 ❎ 🔥 🏥 💗 🔋 🏦 ☁️ ⚡ 🥇
 
 from map import Map
+import time
+import os
+
+TICK_SLEEP = 0.05
 
 tmp = Map(20, 10)
 tmp.generate_forest(3, 10)
 tmp.generate_river(10)
 tmp.generate_river(10)
 tmp.print_map()
+
+tick = 1
+
+while True:
+    os.system('cls')
+    print('TICK', tick)
+    tmp.print_map()
+    tick += 1
+    time.sleep(TICK_SLEEP)
