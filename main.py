5bdee350b8d075e0aee0e83d6ed6d4c0244adc0c
FarhatN <farhat_ink@mail.ru>
2022-06-16 20:12:01 +0500

    добавлена генерация дерева

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import time
 import os
 
 TICK_SLEEP = 0.05
+TREE_UPDATE = 50
 
 tmp = Map(20, 10)
 tmp.generate_forest(3, 10)
@@ -20,3 +21,5 @@ while True:
     tmp.print_map()
     tick += 1
     time.sleep(TICK_SLEEP)
+    if (tick % TREE_UPDATE == 0):
+        tmp.generate_tree()
