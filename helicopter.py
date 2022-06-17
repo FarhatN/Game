ad2200780f97b66c0d65f2a43b842ba9c5c2e643
FarhatN <farhat_ink@mail.ru>
2022-06-17 15:34:59 +0500

    добавлено для клавиш

--- helicopter.py
+++ helicopter.py
@@ -4,6 +4,8 @@ class Helicopter:
     def __init__(self, w, h):
         rc = randcell(w, h)
         rx, ry = rc[0], rc[1]
+        self.h = h
+        self.w = w
         self.x = rx
         self.y = ry
 
