254e0e6e54fca4fe3dd7bf9343279bebd0877ece
FarhatN <farhat_ink@mail.ru>
2022-06-20 19:23:21 +0500

    добавлено для сохранений

--- clouds.py
+++ clouds.py
@@ -7,7 +7,7 @@ class Clouds:
         self.h = h
         self.cells = [[0 for i in range(w)] for j in range(h)]
 
-    def update_clouds(self, r, mxr, g = 4, mxg = 10):
+    def update(self, r = 1, mxr = 20, g = 1, mxg = 10):
         for i in range(self.h):
             for j in range(self.w):
                 if randbool(r, mxr):
@@ -16,3 +16,6 @@ class Clouds:
                         self.cells[i][j] = 2
                 else:
                     self.cells[i][j] = 0
+
+    def export_data(self):
+        return {'cells': self.cells}
